af8f911f3141f1c9a1a85392195baed8e4106013
Lucas Craveiro Paes <lucascraveiropaes123@gmail.com>
2017-12-23 11:59:32 -0200

    Add nova tela de comércio

--- www/assets/js/functions.js
+++ www/assets/js/functions.js
@@ -82,3 +82,12 @@ $(document).on('scroll', function () {
         }
     }
 });
+
+$(function(){
+    var includes = $('[data-include]');
+
+    jQuery.each(includes, function(){
+        var file = $(this).data('include');
+        $(this).load(file);
+    });
+});
