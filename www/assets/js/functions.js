c7ff8f6d4ebca2e3c3fac55874f9f52d2fb7127f
Lucas Craveiro Paes <lucascraveiropaes123@gmail.com>
2018-04-04 02:42:04 -0300

    Criando UI da pesquisa

--- www/assets/js/functions.js
+++ www/assets/js/functions.js
@@ -3,19 +3,31 @@ $('.button-collapse').sideNav({
         edge: 'left', // Choose the horizontal origin
         closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
         draggable: true //, // Choose whether you can drag to open on touch screens,
-        //onOpen: function(el) { /* Do Stuff */ }, 
+        //onOpen: function(el) { /* Do Stuff */ },
         //onClose: function(el) { /* Do Stuff */ },
   	}
 );
 
 $(document).ready(function(){
-  	$('#play').click( function(){
-  		$('#play').toggle(".hide");
-  	});
+    //$('#search-result').append( loader() );
+    $('#search-result').hide();
 
-  	$('#pause').click( function(){
-  		$('#play').toggle(".show");
-  	});
+    $("#search-input").on("change paste keyup", function() {
+        //alert(  );
+        var query = $(this).val();
+        $('#search-result').show();
+
+        console.log("http://localhost:9000/search/"+query,);
+
+        $.ajax({
+            method: "GET",
+            url: "http://localhost:9000/search/"+query,
+        }).done(function( data ) {
+            console.log(data);
+        });
+
+
+    });
 
   	$('.button-search').click( function(){
   		$('#search-bar').addClass("search-show");
@@ -25,28 +37,28 @@ $(document).ready(function(){
   		setTimeout(function(){
   			$('#search-input').focus();
   		}, 100);
-		
+
   	});
 
-  	$('#search-input').focusout( function() {
+  	$('#close-search').click( function() {
 		$('#search-bar').removeClass("search-show");
   		$('.button-search').toggle(".hide");
   		$('.button-collapse').toggle(".hide");
+        $("#search-input").val("");
+        $('#search-result').hide();
   	});
-
-	$('.carousel').carousel();
 });
 
 /* Transição Entre Páginas */
-$(document).ready(function() { 
+$(document).ready(function() {
     $("body").fadeIn(500);
- 
+
     $("a.transition").click(function(event){
         event.preventDefault();
         linkLocation = this.href;
-        $("body").fadeOut(500, redirectPage);      
+        $("body").fadeOut(500, redirectPage);
     });
-         
+
     function redirectPage() {
         window.location = linkLocation;
     }
@@ -56,7 +68,7 @@ $(document).on('scroll', function () {
     if ( $( "nav" ).hasClass( "navbar-image" ) ) {
         var sH = $('.section-top').height();
         var $menu = $('nav.navbar-image');
-        
+
         if ( ( sH - ($menu.height()+56) ) <= $(window).scrollTop() ) {
             var bgURI = $('#section-card-bg').attr('src');
             $('nav').removeClass("transparent");
@@ -65,7 +77,7 @@ $(document).on('scroll', function () {
         } else {
             $('nav').addClass("transparent");
             $('nav').removeClass("background-menu");
-            $('nav').css('background-image', 'none');     
+            $('nav').css('background-image', 'none');
         }
     }
 });
@@ -85,7 +97,7 @@ $.fn.GetURLParameter = function( sParam ){
 
     for (var i = 0; i < sURLVariables.length; i++) {
         var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) { 
+        if (sParameterName[0] == sParam) {
             return decodeURIComponent(sParameterName[1]);
         }
     }
@@ -125,11 +137,6 @@ function formatDate(date1 = "", date2 = "") {
     var day2 = jsDate2.getDate();
     var month2 = (jsDate2.getMonth()+1);
     var year2 = jsDate2.getFullYear();
-
-
-    console.log(day1, day2);
-    console.log(month1, month2);
-    console.log(year1, year2);
 
     var response = "Nos dias "+ day1 +"/"+month1+" até "+ day2 +"/"+month2+" de "+year1;
 
